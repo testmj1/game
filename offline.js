3a28ab1f07e9cd0e19076d2adbb96411aca62b1a
testmj1 <testmjmj192@gmail.com>
2022-12-22 13:12:36 +0530

    game uploade

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1612201553,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/circle-sheet0.png",
+		"images/player-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/buttons-sheet0.png",
+		"images/best-sheet0.png",
+		"images/front-sheet0.png",
+		"images/logo-sheet0.png",
+		"images/buttonfull-sheet0.png",
+		"images/infoscore-sheet0.png",
+		"media/lose.ogg",
+		"media/best.ogg",
+		"media/score.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
